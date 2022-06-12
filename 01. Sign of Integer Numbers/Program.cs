8487a34ca0b6e0caa7e3df146d4714b7d87a29a5
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2022-06-12 23:17:35 +0300

    Add files via upload

---
+++ 01. Sign of Integer Numbers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+
+namespace _01._Sign_of_Integer_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            PrintNum(num);
+        }
+
+        static void PrintNum(int num)
+        {
+            if (num > 0)
+            {
+                Console.WriteLine($"The number {num} is positive. ");
+
+            }
+            else if (num < 0)
+            {
+                Console.WriteLine($"The number {num} is negative. "); 
+
+            }
+            else if (num == 0)
+            {
+                Console.WriteLine($"The number {num} is zero. "); 
+
+            }
+        }
+    }
+}
