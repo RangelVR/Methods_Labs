dbfedc27e7ebd75d41a53f17ddfb55c8eed94269
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2022-06-13 23:25:24 +0300

    Add files via upload

---
+++ 06. Calculate Rectangle Area/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _06._Calculate_Rectangle_Area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            PrintRectangleArea(a, b);
+        }
+
+        static void PrintRectangleArea(int a, int b) 
+        {
+            Console.WriteLine(a * b);
+        }
+    }
+}
