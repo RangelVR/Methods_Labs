458bc45e41734186aa0f5dd5aa17a4b3443cb5e5
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2022-06-13 23:41:25 +0300

    Update Program.cs

--- 07. Repeat String/Program.cs
+++ 07. Repeat String/Program.cs
@@ -21,3 +21,34 @@ namespace _07._Repeat_String
         }
     }
 }
+
+
+//using System;
+//using System.Text;
+
+//namespace _07._Repeat_String
+//{
+//    class Program
+//    {
+//        public static object SrtingBuilder { get; private set; }
+
+//        static void Main(string[] args)
+//        {
+//            string str = Console.ReadLine();
+//            int n = int.Parse(Console.ReadLine());
+
+//            Console.WriteLine(RepeatString(str, n));
+//        }
+
+//        static string RepeatString(string str, int n)
+//        {
+//            StringBuilder sb = new StringBuilder();
+
+//            for (int i = 0; i < n; i++)
+//            {
+//                sb.Append(str);
+//            }
+//            return sb.ToString();
+//        }
+//    }
+//}
