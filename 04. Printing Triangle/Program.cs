d1849f92b5ac056a40651880045a218e716267cc
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2022-06-13 22:41:17 +0300

    Add files via upload

---
+++ 04. Printing Triangle/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _04._Printing_Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            PrintTriangle(n);
+        }
+
+        static void PrintTriangle(int n)
+        {
+            for (int i = 0; i <= n; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write($"{j} ");
+                }
+                Console.WriteLine();
+            }
+            for (int k = n - 1; k > 0; k--)
+            {
+                for (int q = 1; q <= k; q++)
+                {
+                    Console.Write($"{q} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
