f64c0c21b45bf2da78b49fc9b5707e60eaf63916
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2022-06-13 22:42:09 +0300

    Update Program.cs

--- 04. Printing Triangle/Program.cs
+++ 04. Printing Triangle/Program.cs
@@ -31,3 +31,28 @@ namespace _04._Printing_Triangle
         }
     }
 }
+
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        int height = int.Parse(Console.ReadLine());
+    //        for (int i = 1; i < height; i++)
+    //        {
+    //            PrintLine(1, i);
+    //        }
+    //        for (int i = height; i >= 1; i--)
+    //        {
+    //            PrintLine(1, i);
+    //        }
+
+    //    }
+    //    static void PrintLine(int start, int end)
+    //    {
+    //        for (int i = start; i <= end; i++)
+    //        {
+    //            Console.Write(i + " ");
+    //        }
+    //        Console.WriteLine();
+    //    }
+    //}
