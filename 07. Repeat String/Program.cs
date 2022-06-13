0cae0ffc256350c466dd21e29ece55a81076b2c2
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2022-06-13 23:40:28 +0300

    Add files via upload

---
+++ 07. Repeat String/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _07._Repeat_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            int n = int.Parse(Console.ReadLine());
+
+            RepeatString(str, n);
+        }
+
+        static void RepeatString(string str, int n) 
+        {
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(str);
+            }
+        }
+    }
+}
