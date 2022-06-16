73051083541aa44173a65d3972832f8dd4eaae9a
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2022-06-16 22:36:19 +0300

    Add files via upload

---
+++ 11. Math operations/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _11._Math_operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            string operrator = Console.ReadLine(); 
+            int num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(PrintResult(num1, operrator, num2));
+        }
+
+        static int PrintResult(int num1, string operrator, int num2 ) 
+        {
+            int sum = 0;
+            switch (operrator)
+            {
+                case "+":
+                    sum =  num1 + num2;
+                    break;
+                case "-":
+                    sum = num1 - num2;
+                    break;
+                case "*":
+                    sum =  num1 * num2;
+                    break;
+                case "/":
+                    sum = num1 / num2;
+                    break;
+            }
+            return sum;
+        }
+    }
+}
