ab406639b186a2ef84dcec80ea7645adf2d10b4c
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2022-11-08 21:54:53 +0200

    Update Program.cs

--- 02. Grades/Program.cs
+++ 02. Grades/Program.cs
@@ -18,19 +18,19 @@ namespace MathPower
             {
                 Console.WriteLine("Fail");
             }
-            else if (n >= 3 && n < 3.50)
+            else if (n < 3.50)
             {
                 Console.WriteLine("Poor");
             }
-            else if (n >= 3.50 && n < 4.50)
+            else if (n < 4.50)
             {
                 Console.WriteLine("Good");
             }
-            else if (n >= 4.50 && n < 5.50)
+            else if (n < 5.50)
             {
                 Console.WriteLine("Very good");
             }
-            else if (n >= 5.50 && n <= 6.00)
+            else if (n <= 6.00)
             {
                 Console.WriteLine("Excellent");
             }
