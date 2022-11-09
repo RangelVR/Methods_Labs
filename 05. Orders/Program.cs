92691dbba30abc5d1c5a5a77a4193049ce830458
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2022-11-09 22:41:28 +0200

    Update Program.cs

--- 05. Orders/Program.cs
+++ 05. Orders/Program.cs
@@ -1,6 +1,8 @@
-﻿using System;
+using System;
+using System.Linq;
+using System.Text;
 
-namespace _05._Orders
+namespace MathPower
 {
     class Program
     {
@@ -9,26 +11,28 @@ namespace _05._Orders
             string product = Console.ReadLine();
             int quantity = int.Parse(Console.ReadLine());
 
-            PrintTotalPriceOfOrder(product, quantity);
+            Console.WriteLine($"{PrintTotalSum(product, quantity):f2}");
         }
 
-        static void PrintTotalPriceOfOrder(string product, int quantity) 
+        static double PrintTotalSum(string product, int quantity)
         {
+            double totalSum = 0;
             switch (product)
             {
                 case "coffee":
-                    Console.WriteLine($"{(1.50 * quantity):f2}");
+                    totalSum = quantity * 1.50;
                     break;
                 case "water":
-                    Console.WriteLine($"{(1.00 * quantity):f2}");
+                    totalSum = quantity * 1.00;
                     break;
                 case "coke":
-                    Console.WriteLine($"{(1.40 * quantity):f2}");
+                    totalSum = quantity * 1.40;
                     break;
                 case "snacks":
-                    Console.WriteLine($"{(2.00 * quantity):f2}");
+                    totalSum = quantity * 2.00;
                     break;
             }
+            return totalSum;
         }
     }
 }
