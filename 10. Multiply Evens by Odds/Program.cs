db807e9fea7f6aea4996d4402711c6b4358bc728
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2022-11-10 21:24:52 +0200

    Update Program.cs

--- 10. Multiply Evens by Odds/Program.cs
+++ 10. Multiply Evens by Odds/Program.cs
@@ -34,3 +34,38 @@ namespace _10._Multiply_Evens_by_Odds
         }
     }
 }
+
+//using System;
+
+//namespace MathPower
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            int num = Math.Abs(int.Parse(Console.ReadLine()));
+//            Console.WriteLine(MultiplyEvenByOddsDigits(num));
+//        }
+
+//        static int MultiplyEvenByOddsDigits(int num) 
+//        {
+//            int sumEven = 0;
+//            int sumOdd = 0;
+//            while (num !=0)
+//            {
+//                int digit = num % 10;
+//                if (digit % 2 == 0)
+//                {
+//                    sumEven += digit;
+//                }
+//                else
+//                {
+//                    sumOdd += digit;
+//                }
+//                num = num / 10;
+//            }
+//            return sumEven * sumOdd;
+//        }
+
+//    }
+//}
