da803ca40a4e87aaced5a225a7689e231811a7c3
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2023-02-03 22:59:59 +0200

    Update Program.cs

--- 05. Orders/Program.cs
+++ 05. Orders/Program.cs
@@ -1,64 +1,65 @@
-using System;
-
 namespace _05._Orders
 {
-    class Program
+    internal class Program
     {
         static void Main(string[] args)
         {
             string product = Console.ReadLine();
             int quantity = int.Parse(Console.ReadLine());
 
-            PrintTotalPriceOfOrder(product, quantity);
+            GetTotalSum(product, quantity);
         }
 
-        static void PrintTotalPriceOfOrder(string product, int quantity) 
+        static void GetTotalSum(string product, int quantity)
         {
+            double price = 0;
+            double totalSum = 0;
+
             switch (product)
             {
-                case "coffee":
-                    Console.WriteLine($"{(1.50 * quantity):f2}");
-                    break;
-                case "water":
-                    Console.WriteLine($"{(1.00 * quantity):f2}");
-                    break;
-                case "coke":
-                    Console.WriteLine($"{(1.40 * quantity):f2}");
-                    break;
-                case "snacks":
-                    Console.WriteLine($"{(2.00 * quantity):f2}");
-                    break;
+                case "coffee": price = 1.50; break;
+                case "water": price = 1.00; break;
+                case "coke": price = 1.40; break;
+                case "snacks": price = 2.00; break;
             }
+            totalSum = quantity * price;
+
+            Console.WriteLine("{0:f2}", totalSum);
         }
-
-
-        //static void Main(string[] args)
-        //{
-        //    string product = Console.ReadLine();
-        //    int quantity = int.Parse(Console.ReadLine());
-
-        //    Console.WriteLine($"{PrintTotalSum(product, quantity):f2}");
-        //}
-
-        //static double PrintTotalSum(string product, int quantity)
-        //{
-        //    double totalSum = 0;
-        //    switch (product)
-        //    {
-        //        case "coffee":
-        //            totalSum = quantity * 1.50;
-        //            break;
-        //        case "water":
-        //            totalSum = quantity * 1.00;
-        //            break;
-        //        case "coke":
-        //            totalSum = quantity * 1.40;
-        //            break;
-        //        case "snacks":
-        //            totalSum = quantity * 2.00;
-        //            break;
-        //    }
-        //    return totalSum;
-        //}
     }
 }
+
+//using System;
+
+//namespace _05._Orders
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            string product = Console.ReadLine();
+//            int quantity = int.Parse(Console.ReadLine());
+
+//            TotalPriceOfOrder(product, quantity);
+//        }
+
+//        static void TotalPriceOfOrder(string product, int quantity)
+//        {
+//            switch (product)
+//            {
+//                case "coffee":
+//                    Console.WriteLine($"{(1.50 * quantity):f2}");
+//                    break;
+//                case "water":
+//                    Console.WriteLine($"{(1.00 * quantity):f2}");
+//                    break;
+//                case "coke":
+//                    Console.WriteLine($"{(1.40 * quantity):f2}");
+//                    break;
+//                case "snacks":
+//                    Console.WriteLine($"{(2.00 * quantity):f2}");
+//                    break;
+//            }
+//        }
+//    }
+//}
